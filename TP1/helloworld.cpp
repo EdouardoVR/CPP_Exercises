e9d17bc7988cb2cfead629314341c9f910735ae5
Edouardo_Rodrigues <vilarinhorodri@edu.univ-eiffel.fr>
2023-01-12 14:25:35 +0100

    TP1 fini.

--- TP1/helloworld.cpp
+++ TP1/helloworld.cpp
@@ -1,9 +1,15 @@
 #include <iostream>
 
+// using namespace std;
+
 int main(int argc, char const *argv[])
 {
-    std::cout << "Hello world!" << std::endl;
-    int myInt;
-    std::cin >> myInt;
+    // std::cout << "Hello world!" << std::endl;
+    // int myInt;
+    // std::cin >> myInt;
+    std::cout << "What's your name?" << std::endl;
+    std::string myString;
+    std::cin >> myString;
+    std::cout << "Hello " << myString << "!" << std::endl;
     return 0;
 }
