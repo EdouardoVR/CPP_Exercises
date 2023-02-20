8197ab52dad11410309aa3cdf1c6c2d9448df84b
Edouardo_Rodrigues <vilarinhorodri@edu.univ-eiffel.fr>
2023-02-20 17:56:22 +0100

    2 tests validées

--- src/Node.hpp
+++ src/Node.hpp
@@ -1,1 +1,15 @@
 #pragma once
+#include "InstanceCounter.hpp"
+
+#include <iostream>
+
+class Node : public InstanceCounter
+{
+protected:
+    Node() {};
+
+public:
+    virtual ~Node() {};
+    virtual int         value() = 0;
+    virtual std::string print() = 0;
+};
--- src/StringLeaf.hpp
+++ src/StringLeaf.hpp
@@ -1,1 +1,18 @@
 #pragma once
+#include "Leaf.hpp"
+#include "NodeKind.hpp"
+
+#include <iostream>
+#include <string>
+
+class StringLeaf : public Leaf
+{
+public:
+    StringLeaf(std::string value)
+        : _value { value } {};
+
+    NodeKind kind() { return NodeKind::STRING; }
+
+private:
+    std::string _value;
+};
