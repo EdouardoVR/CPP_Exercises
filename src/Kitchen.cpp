461bcac5ade8c9ebe7a202d6fd51f11c670555f5
Edouardo_Rodrigues <vilarinhorodri@edu.univ-eiffel.fr>
2023-03-16 10:18:56 +0100

    TEST 1 passé

--- src/Kitchen.cpp
+++ src/Kitchen.cpp
@@ -1,1 +1,13 @@
 #include "Kitchen.hpp"
+
+const Unit& Kitchen::register_unit(Unit unit)
+{
+    auto [it, emplaced] = _units.emplace(std::move(unit));
+    return *it;
+}
+
+const Unit* Kitchen::find_unit(const std::string& name) const
+{
+    const auto it = _units.find(name);
+    return it != _units.end() ? &(*it) : nullptr;
+}
--- src/Kitchen.hpp
+++ src/Kitchen.hpp
@@ -1,4 +1,26 @@
 #pragma once
+#include "../lib/Unit.hpp"
+
+#include <algorithm>
+#include <set>
+#include <string>
+#include <utility>
 
 class Kitchen
-{};
+{
+public:
+    const Unit& register_unit(Unit unit);
+    const Unit* find_unit(const std::string& name) const;
+
+private:
+    struct ElementNameComparer
+    {
+        using is_transparent = bool;
+
+        bool operator()(const Unit& u1, const Unit& u2) const { return u1.name < u2.name; }
+        bool operator()(const Unit& unit, const std::string& name) const { return unit.name < name; }
+        bool operator()(const std::string& name, const Unit& unit) const { return name < unit.name; }
+    };
+
+    std::set<Unit, ElementNameComparer> _units;
+};
