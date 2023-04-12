26cecb081d18b2207769394dfc15da5e3a4578fa
Edouardo_Rodrigues <vilarinhorodri@edu.univ-eiffel.fr>
2023-04-13 01:33:06 +0200

    fini

--- src/ex1.cpp
+++ src/ex1.cpp
@@ -1,1 +1,25 @@
 #include "ex1.hpp"
+
+void pairwise_concatenate(std::list<std::list<int>>& l1, std::list<std::list<int>>& l2)
+{
+    for (unsigned i = l1.size(); i < l2.size(); ++i)
+        l1.emplace_back();
+    auto it1 = l1.begin();
+    for (auto it2 = l2.begin(); it2 != l2.end(); ++it2)
+    {
+        concatenate(*it1, *it2);
+        ++it1;
+    }
+}
+
+void pairwise_concatenate(std::list<std::list<int>>& l1, std::list<std::list<int>>&& l2)
+{
+    for (unsigned i = l1.size(); i < l2.size(); ++i)
+        l1.emplace_back();
+    auto it1 = l1.begin();
+    for (auto it2 = l2.begin(); it2 != l2.end(); ++it2)
+    {
+        concatenate(*it1, std::move(*it2));
+        ++it1;
+    }
+}
--- src/ex1.hpp
+++ src/ex1.hpp
@@ -1,1 +1,8 @@
 #pragma once
+#include "../lib/concatenate.hpp"
+
+#include <list>
+
+void pairwise_concatenate(std::list<std::list<int>>& l1, std::list<std::list<int>>& l2);
+
+void pairwise_concatenate(std::list<std::list<int>>& l1, std::list<std::list<int>>&& l2);
