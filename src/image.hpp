d39ea116101f33f2893e54377d3cabb248ac07b2
Edouardo_Rodrigues <vilarinhorodri@edu.univ-eiffel.fr>
2023-04-16 23:48:02 +0200

    comm

--- src/image.hpp
+++ src/image.hpp
@@ -1,13 +1,62 @@
 #pragma once
 
+#include "../lib/lib.hpp"
 #include "pixels.hpp"
 
-#include "../lib/lib.hpp"
-
-#include <stdint.h>
+#include <array>
 #include <cstddef>
 #include <functional>
-#include <string>
-#include <array>
 #include <iostream>
+#include <stdint.h>
+#include <string>
 
+template <typename T, size_t largeur, size_t longueur>
+
+struct Image
+{
+private:
+    std::array<std::array<T, largeur>, longueur> _img;
+
+public:
+    Image() = default;
+
+    Image(T pixel)
+    {
+        for (size_t j = 0; j < longueur; j++)
+        {
+            for (size_t i = 0; i < largeur; i++)
+            {
+                _img[j][i] = pixel;
+            }
+        }
+    }
+
+    Image(const std::function<T(const size_t, const size_t)> foo)
+    {
+        for (size_t j = 0; j < longueur; j++)
+        {
+            for (size_t i = 0; i < largeur; i++)
+            {
+                _img[j][i] = foo(i, j);
+            }
+        }
+    }
+
+    const T& operator()(const size_t i, const size_t j) const { return _img[j][i]; }
+
+    T& operator()(const size_t i, const size_t j) { return _img[j][i]; }
+};
+
+template <typename T, size_t largeur, size_t longueur>
+Image<T, largeur, longueur> operator+(Image<T, largeur, longueur> im1, Image<T, largeur, longueur> im2)
+{
+    Image<T, largeur, longueur> img_res;
+    for (size_t j = 0; j < longueur; j++)
+    {
+        for (size_t i = 0; i < largeur; i++)
+        {
+            img_res(i, j) = im1(i, j) + im2(i, j);
+        }
+    }
+    return img_res;
+}
--- src/pixels.hpp
+++ src/pixels.hpp
@@ -4,3 +4,54 @@
 
 #include <stdint.h>
 
+struct RGBA
+{
+    uint8_t r, g, b, a;
+};
+
+struct RGB
+{
+    uint8_t r, g, b;
+};
+
+struct Luma
+{
+    uint8_t gray;
+};
+
+template <typename A, typename B>
+A from(const B&);
+
+template <>
+RGB from(const RGBA& x)
+{
+    return RGB { x.r, x.g, x.b };
+}
+
+template <>
+RGBA from(const RGB& x)
+{
+    return RGBA { x.r, x.g, x.b, 255 };
+}
+
+template <>
+Luma from(const RGB& x)
+{
+    return Luma { lib::rgb_to_grayscale(x.r, x.g, x.b) };
+}
+
+RGBA operator+(const RGBA& x, const RGBA& y)
+{
+    auto [r, g, b, a] = lib::mix_color(x.r, x.g, x.b, x.a, y.r, y.g, y.b, y.a);
+    return RGBA { r, g, b, a };
+}
+
+template <typename A, typename B, typename C>
+#define BLEND(A, B, C)
+A operator+(const B& x, const C& y)
+{
+    return from<A, RGBA>(from<RGBA, B>(x) + from<RGBA, C>(y));
+}
+
+BLEND(RGBA, RGBA, RGB)
+BLEND(RGBA, RGB, RGBA)
